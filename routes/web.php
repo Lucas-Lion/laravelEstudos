37458761152f8a423a27986a3cc9e5672959755c
Lucas-Lion <lucaslion350@gmail.com>
2024-04-29 17:28:10 -0300

    projeto base

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users', [UserController::class, 'index'])->name('');
+route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::get('/', function () {
     return view('welcome');
